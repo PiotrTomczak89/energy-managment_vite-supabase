565864ba2f327e2e33f1bd1a7a25d44106dd1397
piotrektomczakk@gmail.com <PiotrTomczak89>
2023-06-02 15:22:36 +0200

    logOut has been added to Header component

--- src/components/mianSite/Header.jsx
+++ src/components/mianSite/Header.jsx
@@ -1,16 +1,30 @@
 import { useStoreState } from "easy-peasy";
+import { useNavigate } from "react-router-dom";
+import supabase from "../../servives/supabase"
 //import store from "./../../store/store.jsx"
 
 const Header = () => {
-
   const sessionLogin = useStoreState((state) => {
-        return state.sessionLogin
-  })
-//   const test = useStoreState((state) => {
-//     return state.deviceData
-// })
+    return state.sessionLogin;
+  });
+  //   const test = useStoreState((state) => {
+  //     return state.deviceData
+  // })
   const att = sessionLogin.lastIndexOf("@");
   //console.log(test)
+  const navigation = useNavigate();
+
+
+  const handleLogout = async () => {
+    let { error } = await supabase.auth.signOut();
+    if (!error) {
+      navigation("/signin");
+    }
+  };
+
+  // if (!sessionLogin) {
+  //   console.log("test");
+  // }
 
   return (
     <>
@@ -19,14 +33,17 @@ const Header = () => {
           <img src="src/assets/bulbLogo.svg"></img>
         </div>
         <div className="userInformation">
-            <div className="userName"><p>Welcome back</p>
-            <strong>{sessionLogin.substring(0 , att)} !!!</strong></div>
-            {/* //an idea about cost of kWh */}
-            {/* <input placeholder="00000" type="number" style={{width: "10%" , height:"30px" , zIndex: "100"}}></input> */}
+          <div className="userName">
+            <p>Welcome back</p>
+            <strong>{sessionLogin.substring(0, att)} !!!</strong>
+          </div>
+          {/* //an idea about cost of kWh */}
+          {/* <input placeholder="00000" type="number" style={{width: "10%" , height:"30px" , zIndex: "100"}}></input> */}
           <div className="userImage">
-          <span className="material-symbols-outlined avatar">
-account_circle
-</span>
+            <span className="material-symbols-outlined avatar">
+              account_circle
+            </span>
+            <div className="logOut" onClick={handleLogout}>LogOut</div>
           </div>
         </div>
       </header>
