caddb5cb6ea145c777ad78daa3125d72d07dada7
piotrektomczakk@gmail.com <PiotrTomczak89>
2023-06-02 14:49:02 +0200

    style has been implemented to header section

--- src/components/mianSite/Header.jsx
+++ src/components/mianSite/Header.jsx
@@ -9,18 +9,25 @@ const Header = () => {
 //   const test = useStoreState((state) => {
 //     return state.deviceData
 // })
-  console.log(sessionLogin)
+  const att = sessionLogin.lastIndexOf("@");
   //console.log(test)
 
   return (
     <>
       <header className="headerMain">
-        <div className="logo">Logo</div>
+        <div className="logo">
+          <img src="src/assets/bulbLogo.svg"></img>
+        </div>
         <div className="userInformation">
-            <div className="userName">Welcome back {sessionLogin} !!!</div>
+            <div className="userName"><p>Welcome back</p>
+            <strong>{sessionLogin.substring(0 , att)} !!!</strong></div>
             {/* //an idea about cost of kWh */}
             {/* <input placeholder="00000" type="number" style={{width: "10%" , height:"30px" , zIndex: "100"}}></input> */}
-          <div className="userImage">AV</div>
+          <div className="userImage">
+          <span className="material-symbols-outlined avatar">
+account_circle
+</span>
+          </div>
         </div>
       </header>
     </>
