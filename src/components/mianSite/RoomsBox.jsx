4c5b71b49ac9df93caa1497b93390c78eeedb3e5
piotrektomczakk@gmail.com <PiotrTomczak89>
2023-06-09 23:23:12 +0200

    clean RoomsBox component

--- src/components/mianSite/RoomsBox.jsx
+++ src/components/mianSite/RoomsBox.jsx
@@ -1,26 +1,23 @@
-import { useStoreActions, useStoreState } from "easy-peasy";
+import { useStoreActions } from "easy-peasy";
 import supabase from "../../servives/supabase";
 import { useDrop } from "react-dnd";
 import RoomHeader from "./RoomHeader";
 import RoomBody from "./RoomBody";
-import { useEffect , useState } from "react";
 
 const RoomsBox = () => {
-
   const updateDeviceLocation = useStoreActions(
     (actions) => actions.changeDeviceLocation
   );
 
-  const [{isOver, canDrop , dropResult , didDrop } , drop ] = useDrop(() => ({
-  accept: "singleDevice",
-  drop: (item , monitor) => handleUpdateLocation(item , monitor),
-  collect: (monitor) => ({
-  isOver: monitor.isOver(),
-  canDrop: monitor.canDrop(),
-  didDrop: monitor.didDrop(),
-  }),
-}))
-
+  const [{ isOver, canDrop, dropResult, didDrop }, drop] = useDrop(() => ({
+    accept: "singleDevice",
+    drop: (item, monitor) => handleUpdateLocation(item, monitor),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+      didDrop: monitor.didDrop(),
+    }),
+  }));
 
   const handleUpdateLocation = async (item) => {
     const { data, error } = await supabase
@@ -35,21 +32,25 @@ const RoomsBox = () => {
         value: "ROOM1",
       });
     }
-    console.log(item.tab)
-    location.reload() //RELOAD SITE AFTER DROPING ASK ABOUT BETTER SOLUTION
+    location.reload(); //RELOAD SITE AFTER DROPING ASK ABOUT BETTER SOLUTION
   };
 
-
   return (
-    <div className="mainContent__Rooms" >
+    <div className="mainContent__Rooms">
       <div className="mainContent__Rooms__Container">
-                <div className="mainContent__Room"
-                ref={drop}
-                style={{ opacity: isOver ? "80%" : "" , border: isOver ? "3px dashed #528498" : "" , transition:"0.5s", boxShadow: canDrop ? "2px 2px 22px #528498" : ""}}
-                >
-                  <RoomHeader/>
-                  <RoomBody />
-                </div>
+        <div
+          className="mainContent__Room"
+          ref={drop}
+          style={{
+            opacity: isOver ? "80%" : "",
+            border: isOver ? "3px dashed #528498" : "",
+            transition: "0.5s",
+            boxShadow: canDrop ? "2px 2px 22px #528498" : "",
+          }}
+        >
+          <RoomHeader />
+          <RoomBody />
+        </div>
       </div>
     </div>
   );
