17e9feebf16e386334a6957347abfbbf86f810a4
piotrektomczakk@gmail.com <PiotrTomczak89>
2023-06-09 22:53:44 +0200

    clean Content component

--- src/components/mianSite/AllDevicesBox.jsx
+++ src/components/mianSite/AllDevicesBox.jsx
@@ -1,9 +1,7 @@
-import HeaderDevicesBasket from "./HeaderDevicesBasket.jsx"
+import HeaderDevicesBasket from "./HeaderDevicesBasket.jsx";
 import SingleDevice from "./SingleDevice.jsx";
 
 import { useStoreState } from "easy-peasy";
-
-
 
 const AllDevicesBox = () => {
   let devicesFromDataBase = useStoreState((state) => state.deviceData);
@@ -11,11 +9,12 @@ const AllDevicesBox = () => {
   return (
     <div className="mainContent__Input__Box">
       <div className="mainContent__Input__Box--insideBox">
-          <HeaderDevicesBasket />
-          <ul className="deviceContainer">
-          {devicesFromDataBase && devicesFromDataBase.map((singleDevice) => (
-            <SingleDevice key={singleDevice.id} singleDevice={singleDevice}/>
-          ))}
+        <HeaderDevicesBasket />
+        <ul className="deviceContainer">
+          {devicesFromDataBase &&
+            devicesFromDataBase.map((singleDevice) => (
+              <SingleDevice key={singleDevice.id} singleDevice={singleDevice} />
+            ))}
         </ul>
       </div>
     </div>
--- src/components/mianSite/Content.jsx
+++ src/components/mianSite/Content.jsx
@@ -1,61 +1,48 @@
 import Statistic from "./Statistic";
 import Data from "./Data";
 
-import { useEffect , useState } from "react";
-import { useStoreActions , useStoreState } from "easy-peasy";
+import { useStoreActions, useStoreState } from "easy-peasy";
 import supabase from "../../servives/supabase";
-import store from "../../store/store.jsx";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-//useStoreState - read data
-//useStoreAction - save data
+const Content = ({ data }) => {
+  //save data as table of device
+  const importDeviceFromDataBase = useStoreActions(
+    (actions) => actions.importDeviceData
+  );
 
-const Content = ({data}) => {
+  //save data as user email to storage
+  //const createSession = useStoreActions((actions) => actions.importSession(data.session.user.email));
 
-//const [dataBaseData , setDataBasedata] = useState(null);
+  //read data as user email from storage
+  const sessionLogin = useStoreState((state) => {
+    return state.sessionLogin;
+  });
 
-//save data as table of device
-const importDeviceFromDataBase = useStoreActions((actions) => actions.importDeviceData);
+  //download filtred data from supabase (filter equal to user login)
+  const getDataFromDataBase = async () => {
+    let { data, error } = await supabase
+      .from("deviceTable")
+      .select("*")
+      .eq("author", sessionLogin);
 
-
-//save data as user email to storage
-const createSession = useStoreActions((actions) => actions.importSession(data.session.user.email));
-
-//read data as user email from storage
-const sessionLogin = useStoreState((state) => {
-  return state.sessionLogin
-})
-
-//download filtred data from supabase (filter equal to user login)
-const getDataFromDataBase = async () => {
-  let { data , error } = await supabase
-.from('deviceTable')
-.select("*").eq("author" , sessionLogin);;
-
-if (!error) {
-  //setDataBasedata(data)
-  importDeviceFromDataBase(data);
-}
-
-}
-//console.log(dataBaseData , "content")
-
-//console.log(dataBaseData)
-
-//run function that put data into store
-getDataFromDataBase()
+    if (!error) {
+      importDeviceFromDataBase(data);
+    }
+  };
+  //run function that put data into store
+  getDataFromDataBase();
 
   return (
-      <section className="mainContent">
-        {/* section left bar */}
-        <Statistic/>
-        {/* section right bar  */}
-        <DndProvider backend={HTML5Backend}>
+    <section className="mainContent">
+      {/* section left bar */}
+      <Statistic />
+      {/* section right bar  */}
+      <DndProvider backend={HTML5Backend}>
         <Data />
-        </DndProvider>
-        
-      </section>
+      </DndProvider>
+    </section>
   );
 };
 
