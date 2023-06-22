0b3f6a0a5b925ed3d6a868802170a323de5abcb3
piotrektomczakk@gmail.com <PiotrTomczak89>
2023-06-22 12:02:30 +0200

    border radius has been added to git logo

--- src/components/mianSite/Footer.jsx
+++ src/components/mianSite/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 return (
     <footer>
         <h2>App Consumption Counter kWh</h2>
-        <div><a href="https://github.com/PiotrTomczak89">© 2023 Tomczak Piotr <img width="50px" height="50px" src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png"></img></a></div>
+        <div><a href="https://github.com/PiotrTomczak89">© 2023 Tomczak Piotr <img width="50px" height="50px" style={{borderRadius:"50%"}} src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png"></img></a></div>
         <div>v. 1.0.0</div>
     </footer>
 )
